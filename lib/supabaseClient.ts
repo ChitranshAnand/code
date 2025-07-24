822269ae7170642dbf207d18618b3bda730243e7
ChitranshAnand <chitranshsinha540@gmail.com>
2025-07-25 03:31:02 +0530

    removed comments

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -11,4 +11,3 @@ export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 )
 console.log('Supabase URL ->', process.env.NEXT_PUBLIC_SUPABASE_URL)
-// console.log('Supabase Anon Key ->', process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY)
